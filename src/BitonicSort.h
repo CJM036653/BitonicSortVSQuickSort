6463264a756edbc8db38a31f10ac3879ab3bc5b8
Matteo Perin <matteoperin1302@gmail.com>
2019-07-12 19:03:18 +0200

    Upload prima versione

--- src/BitonicSort.h
+++ src/BitonicSort.h
@@ -8,4 +8,19 @@
 #define FALSE 0
 #endif
 
+/*Compare two values and swap base on direction:
+true -> swap if a > b
+false -> swap if a <= b*/
+void compare(int *a, int *b, bool dir);
+
+/*Sort a sequence in ascending order if dir = 1 or descending if dir = 0*/
+void bitonicMerge(int a[], int low, int dim, bool dir);
+
+void bitonicSort(int a[], int low, int dim, bool dir);
+
+void sort(int a[], int dim);
+
+void mergeAndSplit(int in[], int rank, int r_min, int r_max, int num_keys);
+
+
 #endif
